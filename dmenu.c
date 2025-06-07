15aeb07391c978eb5caebcbd52815f600598438f
prinixoz <prinixoz@proton.me>
2025-06-08 02:15:03 +0530

    Error v which comes with ctrl+v fixed

--- dmenu.c
+++ dmenu.c
@@ -155,7 +155,7 @@ void keypress(struct dmenu_panel *panel, enum wl_keyboard_key_state state,
                    cursor = strlen(text_);
 
                    free(texts);
-                   sym = XKB_KEY_End;
+                   sym = XKB_KEY_NoSymbol;
                }
            }
         break;
